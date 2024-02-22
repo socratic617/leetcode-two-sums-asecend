c037b7ee4f5033b1e771f0b5ae53e5ad8e800f67
socratic617 <erik.solis02135@gmail.com>
2024-02-21 21:03:45 -0500

    testing solution for jest

--- problem-list.test.js
+++ problem-list.test.js
@@ -14,3 +14,9 @@
 //nums[2,7,11,15]
 //target 9
 
+
+const twoSums = require('./problem-list.js')
+
+test('Given an array of integers, nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.', () => {
+  expect(twoSums([2, 7, 11, 15], 9)).toBe([0, 1])
+})
