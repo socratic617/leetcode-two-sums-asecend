2983919c3683e16ce7693b0b96dda2dca6ee2bec
socratic617 <erik.solis02135@gmail.com>
2024-02-21 21:00:41 -0500

    psuedo code, 3 test cases, target solution

---
+++ problem-list.js
@@ -0,0 +1,11 @@
+// const twoSums  = require('/problem-list')
+// test('Given[4,5,6] and target of 11, returns [1,2]', () => {
+//  const nums = [4,5,6]
+//  const target = 11
+//   let solution = []
+//   expect(solution).toBe([1,2])
+// })
+
+// module.exports= twoSums
+
+
