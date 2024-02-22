f3b30f655ea5c82e4af3d6f3c8882b73bddad5e0
socratic617 <erik.solis02135@gmail.com>
2024-02-21 21:04:17 -0500

    testing for solution only works for toEqual

--- problem-list.test.js
+++ problem-list.test.js
@@ -18,5 +18,5 @@
 const twoSums = require('./problem-list.js')
 
 test('Given an array of integers, nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.', () => {
-  expect(twoSums([2, 7, 11, 15], 9)).toBe([0, 1])
+  expect(twoSums([2, 7, 11, 15], 9)).toEqual([0, 1])
 })
