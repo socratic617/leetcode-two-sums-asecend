3bad7222ad2a52fc83aa7d54115b8964905b017a
socratic617 <erik.solis02135@gmail.com>
2024-02-21 21:02:50 -0500

    setting up enviornment for Jest , test cases psuedo code

---
+++ problem-list.test.js
@@ -0,0 +1,16 @@
+//pesudo code : Test 1
+// Given :
+// nums = [1,2,3]
+// target = 5
+// return is expected to be = [1,2]
+
+
+// Test 2:
+// nums[4,5,6]
+// target=11
+// return=[1,2]
+
+// Test 3:
+//nums[2,7,11,15]
+//target 9
+
